0c49a8a37bb1d198ee7089550ed841a717b69a26
hudsonchromy <hjc0018@auburn.edu>
2019-07-03 09:32:04 -0500

    initial commit

--- test.php
+++ test.php
@@ -2,5 +2,5 @@
 	$HOST = getenv('host');
 	$USERNAME = getenv('username');
 	$PASSWORD = getenv('password');
-	$db = mysqli_connect($HOST, $USERNAME, $PASSWORD, 'ForkIt') or die('Error: Unable to Connect');
+	$db = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'bcd2f7930ec787', '06fbf25c', 'heroku_85857c074ef2ccf', 3306); or die('Error: Unable to Connect');
 ?>
