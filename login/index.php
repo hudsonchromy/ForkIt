351c054789fd094f834c81216c5a01c7ac16fc24
hudsonchromy <hjc0018@auburn.edu>
2019-07-03 12:46:50 -0500

    fixing login

--- login/index.php
+++ login/index.php
@@ -3,7 +3,6 @@
 	<head>
 		<title>ForkIt - Login</title>
 		<link rel="stylesheet" type="text/css" href="../style.css">
-		<link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
 	</head>
 	<body>
 		<a href="/">
