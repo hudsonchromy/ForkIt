f9bd4bde8571a87e263e1bc6068982c296e34e79
hudsonchromy <hjc0018@auburn.edu>
2019-07-03 09:34:26 -0500

    initial commit

--- test.php
+++ test.php
@@ -2,5 +2,5 @@
 	$HOST = getenv('host');
 	$USERNAME = getenv('username');
 	$PASSWORD = getenv('password');
-	$db = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'bcd2f7930ec787', '06fbf25c', 'heroku_85857c074ef2ccf', 3306); or die('Error: Unable to Connect');
+	$db = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'b2a6cb9657881f', 'c0f2177', 'heroku_0eff3e82d762b77', 3306); or die('Error: Unable to Connect');
 ?>
